b4ec89c96929be209ae24a53d6b05da017c8144b
Vladimir Novoselskiy <vladimirnovoselskiy@vladimirs-macbook-pro.local>
2019-05-23 00:10:57 -0700

    wrote the logic for the calculator. user enters an amount, selects tip and out comes a bill based on the tip and the bill amount

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -9,12 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var total_label: UILabel!
+    
 
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    
+ 
+    
+    @IBOutlet weak var tipcontrol: UISegmentedControl!
+    
+    @IBOutlet weak var Labelfield: UILabel!
+    
+    @IBOutlet weak var billfield: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true)    }
 
-
+    @IBAction func calculate(_ sender: Any) {
+        
+        let tipPercentage = [0.18,0.20,0.25]
+        
+        let bill = Double(billfield.text!) ?? 0
+        
+        let tip = bill * tipPercentage[tipcontrol.selectedSegmentIndex]
+        
+        let total = bill + tip
+        
+        tipLabel.text = String(format:"$%.2f",tip)
+        total_label.text = String(format:"$%.2f", total)
+        
+        
+        
+        
+        
+        
+        //total_label.text = "$110.00"
+       // tipLabel.text = "20.00"
+    }
 }
+
+
+      // let bill = Double(billfield.text!) ?? 0
+        
+      //  let tip = bill * 0.2
+       // let total = bill + tip
+        //tipLabel.text = String(format:"$%.2f",tip)
+        
+       // let tipPercent = [0.1, 0.18, 0.2]
+        
+        //let tip = bill * tipPercent[tipcontrol.selectedSegmentIndex]
+        
+       // let total = bill + tip
+
+
+
+
+
 
